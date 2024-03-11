0f787d11f66c6e48d4b3aeb30c9b4d6beab11b3f
lexi <alexiacabanel1@gmail.com>
2024-03-11 14:19:42 +0100

    validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_url, presence: true
   has_many :cart_products
   has_many :carts, through: :cart_products
 end
