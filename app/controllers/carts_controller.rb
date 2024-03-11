23331f8cf581c9eb52409afd662e4f054e2c7a85
WinnyDrancourt <winny.morel@orange.fr>
2024-03-11 16:10:35 +0100

    cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,22 @@
 class CartsController < ApplicationController
+  def show
+    @cart = Cart.find(params['id'])
+  end
+
+  def new
+    @cart = Cart.new
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+  end
+
+  private
+
+  def cart_params
+    cart_params = params.require(:cart).permit(:user, :product)
+    cart_params[:product] = Product.find(cart_params[:product].to_i)
+    cart_params[:user] = User.find(cart_params[:user].to_i)
+    cart_params
+  end
 end
