6aeda56e6ae8844400515f9e8885303801072e90
WinnyDrancourt <winny.morel@orange.fr>
2024-03-12 18:05:12 +0100

    fix sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params
 
   # GET /resource/sign_up
@@ -42,9 +42,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[last_name first_name town zip_code adress])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
