af32c8bdc4d432771590581ca04a1bad64a467d7
ShaunChowthee <sn.chowthee@gmail.com>
2024-03-12 10:47:20 +0100

    fixed double root path in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
      post 'add/:product_id', to: 'carts#add', as: 'add_to'
   end
 
-  root to: 'index#products'
+  root to: 'products#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
